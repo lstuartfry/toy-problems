2e8d788a9235183a8853d11ae952d9ac11ec8269
Lukas Stuart-Fry <lstuartfry@gmail.com>
2016-09-14 19:16:48 -0700

    [add] adds first commit, LeetCode-lengthOfLongestString

---
+++ LeetCode/lengthOfLongestSubstring.js
@@ -0,0 +1,13 @@
+var lengthOfLongestSubstring = function(s) {
+  var longestString = "";
+  var currentString = "";
+  for (var i = 1; i < s.length; i++) {
+    if (s[i] !== s[i - 1] && currentString.indexOf(s[i]) === -1) {
+      currentString = currentString + s[i];
+      if (currentString.length > longestString.length) {
+        longestString = currentString;
+      }
+    }
+  }
+  return longestString;
+};
