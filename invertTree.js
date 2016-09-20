78b32d72ca7872c51efc32e776f519dea261ca22
Lukas Stuart-Fry <lstuartfry@gmail.com>
2016-09-20 13:40:47 -0700

    [add] adds invertTree problem and solution

---
+++ invertTree.js
@@ -0,0 +1,13 @@
+/*
+Invert a binary tree.
+*/
+
+var invertTree = function(root) {
+	if(root) {
+		var left = root.left ? root.left : null;
+		var right = root.right ? root.right : null;
+		root.left = invertTree(right);
+		root.right = invertTree(left);
+	}
+	return root;
+};
