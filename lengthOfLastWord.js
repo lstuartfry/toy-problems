2d5f70b728fada1c72b34b4322493353480dfd82
Lukas Stuart-Fry <lstuartfry@gmail.com>
2016-09-22 15:07:29 -0700

    [add] adds lengthOfLastWord.js problem and solution

---
+++ lengthOfLastWord.js
@@ -0,0 +1,22 @@
+/*
+Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
+return the length of last word in the string.
+
+If the last word does not exist, return 0.
+*/
+
+var lengthOfLastWord = function(s) {
+    if(s.length) {
+    var array = s.split(" ").filter(function(item) {
+    	return item !== "";
+    });
+    if(array[array.length-1] !== null && array.length) {
+        var result = array[array.length-1];
+        return result.length;
+    } else {
+        return 0;
+    }
+    } else {
+    	return 0;
+    }
+};
