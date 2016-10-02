c3841a5a26b0b169c14493fe4ec8eb846a8be139
Lukas Stuart-Fry <lstuartfry@gmail.com>
2016-10-02 15:18:25 -0700

    [update] updates deleteDuplicates.js PART I with detailed comments

--- deleteDuplicates.js
+++ deleteDuplicates.js
@@ -9,9 +9,14 @@ Given 1->1->2->3->3, return 1->2->3.
 
 var deleteDuplicates = function(head) {
 	var current = head;
+	// while the current node is valid, continue with if/else handling of each node
     while(current) {
+    	// if the current node's .next value is valid, and is equal to the current node's .val,
+    	// we have found duplicate values, and can "erase" the second instance of the duplicate
+    	// by skipping over the current.next value, and setting it instead to the next node in the sequence
     	if(current.next !== null && current.val === current.next.val) {
     		current.next = current.next.next;
+    	// otherwise if no duplicate is found, step through to the next node in the list
     	} else {
     		current = current.next;
     	}
