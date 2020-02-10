50d55283c0405b4dce68c8380518c8edc577cc59
Lukas Stuart-Fry <lstuartfry@gmail.com>
2020-02-10 14:13:49 -0800

    updated balancedParens, removes redundant validParentheses

---
+++ balancedParens.js
@@ -0,0 +1,42 @@
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', 
+determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid 
+but "(]" and "([)]" are not.
+*/
+
+const balancedParens = string => {
+	// first we create an object that references the proper relationship
+	// between our various parentheses characters
+	const pairs = {
+		'(': ')',
+		'{': '}',
+		'[': ']',
+	};
+
+	// we will use a stack method to help us keep track of which parentheses characters we've
+	// come across in the string
+	let stack = [];
+
+	// loop through the string
+	for (const val of string) {
+		// if our pairs object has a key-value matching the current string's character,
+		// push that character into our stack.  You will notice that ONLY closed-parentheses characters
+		// get added to our stack.
+		if (pairs[val]) {
+			stack.push(pairs[val]);
+		}
+		// when we come across any closed-parentheses characters in our string,
+		// our string is ONLY balanced if we pop off the most recently-added character in our
+		// stack, and the characters match
+		else {
+			if (stack.pop() !== val) {
+				return false;
+			}
+		}
+	}
+
+	// if we have a truly balanced string, our stack will be empty after completing our for-loop
+	return stack.length === 0;
+};
